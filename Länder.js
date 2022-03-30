6e09be673e78b0eed0bb89ca9b1c48cd3d146156
BambisNutsack <102255604+BambisNutsack@users.noreply.github.com>
2022-03-30 15:55:48 +0200

    BOI

---
+++ Länder.js
@@ -0,0 +1,302 @@
+const Länder = [{
+        code: "al.svg",
+        latitude: 41.153332,
+        longitude: 20.168331,
+        name: "Albanien",
+    },
+    {
+        code: "ad.svg",
+        latitude: 42.546245,
+        longitude: 1.601554,
+        name: "Andorra",
+    },
+    {
+        code: "by.svg",
+        latitude: 53.709807,
+        longitude: 27.953389,
+        name: "Weißrussland",
+    },
+    {
+        code: "be.svg",
+        latitude: 50.503887,
+        longitude: 4.469936,
+        name: "Belgien",
+    },
+    {
+        code: "ba.svg",
+        latitude: 43.915886,
+        longitude: 17.679076,
+        name: "Bosnien und Herzegowina",
+    },
+    {
+        code: "bg.svg",
+        latitude: 42.733883,
+        longitude: 25.48583,
+        name: "Bulgarien",
+    },
+    {
+        code: "dk.svg",
+        latitude: 56.26392,
+        longitude: 9.501785,
+        name: "Dänemark",
+    },
+    {
+        code: "de.svg",
+        latitude: 51.165691,
+        longitude: 10.451526,
+        name: "Deutschland",
+    },
+    {
+        code: "ee.svg",
+        latitude: 58.595272,
+        longitude: 25.013607,
+        name: "Estland",
+    },
+    {
+        code: "fi.svg",
+        latitude: 61.92411,
+        longitude: 25.748151,
+        name: "Finnland",
+    },
+    {
+        code: "fr.svg",
+        latitude: 46.227638,
+        longitude: 2.213749,
+        name: "Frankreich",
+    },
+    {
+        code: "gr.svg",
+        latitude: 39.074208,
+        longitude: 21.824312,
+        name: "Griechenland",
+    },
+    {
+        code: "ie.svg",
+        latitude: 53.41291,
+        longitude: -8.24389,
+        name: "Irland",
+    },
+    {
+        code: "is.svg",
+        latitude: 64.963051,
+        longitude: -19.020835,
+        name: "Island",
+    },
+    {
+        code: "it.svg",
+        latitude: 41.87194,
+        longitude: 12.56738,
+        name: "Italien",
+    },
+    {
+        code: "kz.svg",
+        latitude: 48.019573,
+        longitude: 66.923684,
+        name: "Kasachstan",
+    },
+    {
+        code: "hr.svg",
+        latitude: 45.1,
+        longitude: 15.2,
+        name: "Kroatien",
+    },
+    {
+        code: "lv.svg",
+        latitude: 56.879635,
+        longitude: 24.603189,
+        name: "Lettland",
+    },
+    {
+        code: "li.svg",
+        latitude: 47.166,
+        longitude: 9.555373,
+        name: "Liechtenstein",
+    },
+    {
+        code: "lt.svg",
+        latitude: 55.169438,
+        longitude: 23.881275,
+        name: "Litauen",
+    },
+    {
+        code: "lu.svg",
+        latitude: 49.815273,
+        longitude: 6.129583,
+        name: "Luxemburg",
+    },
+    {
+        code: "mt.svg",
+        latitude: 35.937496,
+        longitude: 14.375416,
+        name: "Malta",
+    },
+    {
+        code: "md.svg",
+        latitude: 47.411631,
+        longitude: 28.369885,
+        name: "Moldau",
+    },
+    {
+        code: "mc.svg",
+        latitude: 43.750298,
+        longitude: 7.412841,
+        name: "Monaco",
+    },
+    {
+        code: "me.svg",
+        latitude: 42.708678,
+        longitude: 19.37439,
+        name: "Montenegro",
+    },
+    {
+        code: "nl.svg",
+        latitude: 52.132633,
+        longitude: 5.291266,
+        name: "Niederlande",
+    },
+    {
+        code: "mk.svg",
+        latitude: 41.608635,
+        longitude: 21.745275,
+        name: "Nord Mazedonien",
+    },
+    {
+        code: "no.svg",
+        latitude: 60.472024,
+        longitude: 8.468946,
+        name: "Norwegen",
+    },
+    {
+        code: "au.svg",
+        latitude: -25.274398,
+        longitude: 133.775136,
+        name: "Österreich",
+    },
+    {
+        code: "pl.svg",
+        latitude: 51.919438,
+        longitude: 19.145136,
+        name: "Polen",
+    },
+    {
+        code: "pt.svg",
+        latitude: 39.399872,
+        longitude: -8.224454,
+        name: "Portugal",
+    },
+    {
+        code: "ro.svg",
+        latitude: 45.943161,
+        longitude: 24.96676,
+        name: "Rumänien",
+    },
+    {
+        code: "ru.svg",
+        latitude: 61.52401,
+        longitude: 105.318756,
+        name: "Russland",
+    },
+    {
+        code: "sm.svg",
+        latitude: 43.94236,
+        longitude: 12.457777,
+        name: "San Marino",
+    },
+    {
+        code: "se.svg",
+        latitude: 60.128161,
+        longitude: 18.643501,
+        name: "Schweden",
+    },
+    {
+        code: "ch.svg",
+        latitude: 46.818188,
+        longitude: 8.227512,
+        name: "Schweiz",
+    },
+    {
+        code: "rs.svg",
+        latitude: 44.016521,
+        longitude: 21.005859,
+        name: "Serbien",
+    },
+    {
+        code: "sk.svg",
+        latitude: 48.669026,
+        longitude: 19.699024,
+        name: "Slowakei",
+    }, {
+        code: "si.svg",
+        latitude: 46.151241,
+        longitude: 14.995463,
+        name: "Slowenien",
+    },
+    {
+        code: "es.svg",
+        latitude: 40.463667,
+        longitude: -3.74922,
+        name: "Spanien",
+    },
+    {
+        code: "cz.svg",
+        latitude: 49.817492,
+        longitude: 15.472962,
+        name: "Tschechien",
+    },
+    {
+        code: "tr.svg",
+        latitude: 38.963745,
+        longitude: 35.243322,
+        name: "Türkei",
+    }, {
+        code: "ua.svg",
+        latitude: 48.379433,
+        longitude: 31.16558,
+        name: "Ukraine",
+    },
+    {
+        code: "hu.svg",
+        latitude: 47.162494,
+        longitude: 19.503304,
+        name: "Ungarn",
+    },
+    {
+        code: "va.svg",
+        latitude: 41.902916,
+        longitude: 12.453389,
+        name: "Vatikanstadt",
+    },
+    {
+        code: "gb.svg",
+        latitude: 55.378051,
+        longitude: -3.435973,
+        name: "Vereinigtes Königreich",
+    }
+];
+
+const imgArr = Länder.map(({ code: actualValue }) => actualValue);
+window.onload = Autocomplete(), LandAuswahl()
+
+// Erstellen der Datenlisten optionen
+function Autocomplete() {
+    var dataList = document.getElementById("LänderListe");
+    var input = document.getElementById("LänderEingabe");
+    input.placeholder = "Ländername";
+    for (var i = 0; i < Länder.length; i++) {
+        dataList.innerHTML = dataList.innerHTML +
+            '<option value="' + Länder[i].name + '"></option>';
+    }
+}
+
+// Länderauswahl    -------------------------------
+function LandAuswahl() {
+    var num = Math.floor(Math.random() * (imgArr.length));
+    document.canvas.src = "img/" + imgArr[num];
+}
+
+// Theme    --------------------------------------
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
