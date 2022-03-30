29022775df05509a6c1fe9e0db8e2383e9a479a9
BambisNutsack <102255604+BambisNutsack@users.noreply.github.com>
2022-03-30 16:44:35 +0200

    boi

--- Länder.js
+++ Länder.js
@@ -284,7 +284,7 @@ function Autocomplete() {
     input.placeholder = "Ländername";
     for (var i = 0; i < Länder.length; i++) {
         dataList.innerHTML = dataList.innerHTML +
-            '<option value="' + Länder[i].name + '"></option>';
+            "<option value=" + Länder[i].name + "></option>";
     }
 }
 
@@ -295,8 +295,46 @@ function LandAuswahl() {
 }
 
 // Theme    --------------------------------------
-const chk = document.getElementById('chk');
+const chk = document.getElementById("chk");
 
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
+chk.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
 });
+
+// function distance(lat1,
+//     lat2, lon1, lon2)
+// {
+
+// // The math module contains a function
+// // named toRadians which converts from
+// // degrees to radians.
+// lon1 =  lon1 * Math.PI / 180;
+// lon2 = lon2 * Math.PI / 180;
+// lat1 = lat1 * Math.PI / 180;
+// lat2 = lat2 * Math.PI / 180;
+
+// // Haversine formula
+// let dlon = lon2 - lon1;
+// let dlat = lat2 - lat1;
+// let a = Math.pow(Math.sin(dlat / 2), 2)
+// + Math.cos(lat1) * Math.cos(lat2)
+// * Math.pow(Math.sin(dlon / 2),2);
+
+// let c = 2 * Math.asin(Math.sqrt(a));
+
+// // Radius of earth in kilometers. Use 3956
+// // for miles
+// let r = 6371;
+
+// // calculate the result
+// return(c * r);
+// }
+
+// // Driver code   
+
+// let lat1 = 53.32055555555556;
+// let lat2 = 53.31861111111111;
+// let lon1 = -1.7297222222222221;
+// let lon2 = -1.6997222222222223;
+// document.write(distance(lat1, lat2,
+//           lon1, lon2) + " K.M");
