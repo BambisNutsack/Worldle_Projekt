c3dc5692872a89ccb9b41fe6aea5479f60784f7a
BambisNutsack <102255604+BambisNutsack@users.noreply.github.com>
2022-04-06 21:28:32 +0200

    boi

--- Länder.js
+++ Länder.js
@@ -1,274 +1,274 @@
 // images => https://github.com/djaiss/mapsicon
 const Länder = [{
-        code: "al.svg",
+        code: "al",
         latitude: 41.153332,
         longitude: 20.168331,
         name: "Albanien",
     },
     {
-        code: "ad.svg",
+        code: "ad",
         latitude: 42.546245,
         longitude: 1.601554,
         name: "Andorra",
     },
     {
-        code: "by.svg",
+        code: "by",
         latitude: 53.709807,
         longitude: 27.953389,
         name: "Weißrussland",
     },
     {
-        code: "be.svg",
+        code: "be",
         latitude: 50.503887,
         longitude: 4.469936,
         name: "Belgien",
     },
     {
-        code: "ba.svg",
+        code: "ba",
         latitude: 43.915886,
         longitude: 17.679076,
         name: "Bosnien und Herzegowina",
     },
     {
-        code: "bg.svg",
+        code: "bg",
         latitude: 42.733883,
         longitude: 25.48583,
         name: "Bulgarien",
     },
     {
-        code: "dk.svg",
+        code: "dk",
         latitude: 56.26392,
         longitude: 9.501785,
         name: "Dänemark",
     },
     {
-        code: "de.svg",
+        code: "de",
         latitude: 51.165691,
         longitude: 10.451526,
         name: "Deutschland",
     },
     {
-        code: "ee.svg",
+        code: "ee",
         latitude: 58.595272,
         longitude: 25.013607,
         name: "Estland",
     },
     {
-        code: "fi.svg",
+        code: "fi",
         latitude: 61.92411,
         longitude: 25.748151,
         name: "Finnland",
     },
     {
-        code: "fr.svg",
+        code: "fr",
         latitude: 46.227638,
         longitude: 2.213749,
         name: "Frankreich",
     },
     {
-        code: "gr.svg",
+        code: "gr",
         latitude: 39.074208,
         longitude: 21.824312,
         name: "Griechenland",
     },
     {
-        code: "ie.svg",
+        code: "ie",
         latitude: 53.41291,
         longitude: -8.24389,
         name: "Irland",
     },
     {
-        code: "is.svg",
+        code: "is",
         latitude: 64.963051,
         longitude: -19.020835,
         name: "Island",
     },
     {
-        code: "it.svg",
+        code: "it",
         latitude: 41.87194,
         longitude: 12.56738,
         name: "Italien",
     },
     {
-        code: "kz.svg",
+        code: "kz",
         latitude: 48.019573,
         longitude: 66.923684,
         name: "Kasachstan",
     },
     {
-        code: "hr.svg",
+        code: "hr",
         latitude: 45.1,
         longitude: 15.2,
         name: "Kroatien",
     },
     {
-        code: "lv.svg",
+        code: "lv",
         latitude: 56.879635,
         longitude: 24.603189,
         name: "Lettland",
     },
     {
-        code: "li.svg",
+        code: "li",
         latitude: 47.166,
         longitude: 9.555373,
         name: "Liechtenstein",
     },
     {
-        code: "lt.svg",
+        code: "lt",
         latitude: 55.169438,
         longitude: 23.881275,
         name: "Litauen",
     },
     {
-        code: "lu.svg",
+        code: "lu",
         latitude: 49.815273,
         longitude: 6.129583,
         name: "Luxemburg",
     },
     {
-        code: "mt.svg",
+        code: "mt",
         latitude: 35.937496,
         longitude: 14.375416,
         name: "Malta",
     },
     {
-        code: "md.svg",
+        code: "md",
         latitude: 47.411631,
         longitude: 28.369885,
         name: "Moldau",
     },
     {
-        code: "mc.svg",
+        code: "mc",
         latitude: 43.750298,
         longitude: 7.412841,
         name: "Monaco",
     },
     {
-        code: "me.svg",
+        code: "me",
         latitude: 42.708678,
         longitude: 19.37439,
         name: "Montenegro",
     },
     {
-        code: "nl.svg",
+        code: "nl",
         latitude: 52.132633,
         longitude: 5.291266,
         name: "Niederlande",
     },
     {
-        code: "mk.svg",
+        code: "mk",
         latitude: 41.608635,
         longitude: 21.745275,
         name: "Nord Mazedonien",
     },
     {
-        code: "no.svg",
+        code: "no",
         latitude: 60.472024,
         longitude: 8.468946,
         name: "Norwegen",
     },
     {
-        code: "au.svg",
+        code: "au",
         latitude: -25.274398,
         longitude: 133.775136,
         name: "Österreich",
     },
     {
-        code: "pl.svg",
+        code: "pl",
         latitude: 51.919438,
         longitude: 19.145136,
         name: "Polen",
     },
     {
-        code: "pt.svg",
+        code: "pt",
         latitude: 39.399872,
         longitude: -8.224454,
         name: "Portugal",
     },
     {
-        code: "ro.svg",
+        code: "ro",
         latitude: 45.943161,
         longitude: 24.96676,
         name: "Rumänien",
     },
     {
-        code: "ru.svg",
+        code: "ru",
         latitude: 61.52401,
         longitude: 105.318756,
         name: "Russland",
     },
     {
-        code: "sm.svg",
+        code: "sm",
         latitude: 43.94236,
         longitude: 12.457777,
         name: "San Marino",
     },
     {
-        code: "se.svg",
+        code: "se",
         latitude: 60.128161,
         longitude: 18.643501,
         name: "Schweden",
     },
     {
-        code: "ch.svg",
+        code: "ch",
         latitude: 46.818188,
         longitude: 8.227512,
         name: "Schweiz",
     },
     {
-        code: "rs.svg",
+        code: "rs",
         latitude: 44.016521,
         longitude: 21.005859,
         name: "Serbien",
     },
     {
-        code: "sk.svg",
+        code: "sk",
         latitude: 48.669026,
         longitude: 19.699024,
         name: "Slowakei",
     }, {
-        code: "si.svg",
+        code: "si",
         latitude: 46.151241,
         longitude: 14.995463,
         name: "Slowenien",
     },
     {
-        code: "es.svg",
+        code: "es",
         latitude: 40.463667,
         longitude: -3.74922,
         name: "Spanien",
     },
     {
-        code: "cz.svg",
+        code: "cz",
         latitude: 49.817492,
         longitude: 15.472962,
         name: "Tschechien",
     },
     {
-        code: "tr.svg",
+        code: "tr",
         latitude: 38.963745,
         longitude: 35.243322,
         name: "Türkei",
     }, {
-        code: "ua.svg",
+        code: "ua",
         latitude: 48.379433,
         longitude: 31.16558,
         name: "Ukraine",
     },
     {
-        code: "hu.svg",
+        code: "hu",
         latitude: 47.162494,
         longitude: 19.503304,
         name: "Ungarn",
     },
     {
-        code: "va.svg",
+        code: "va",
         latitude: 41.902916,
         longitude: 12.453389,
         name: "Vatikanstadt",
     },
     {
-        code: "gb.svg",
+        code: "gb",
         latitude: 55.378051,
         longitude: -3.435973,
         name: "Vereinigtes Königreich",
@@ -276,31 +276,6 @@ const Länder = [{
 ];
 
 
-// svg array
-const imgArr = Länder.map(({ code: actualValue }) => actualValue);
-// laden der funktionen 
-window.onload = Autocomplete(), LandAuswahl()
-
-
-
-// datenlisten optionen     --------------------------------------
-function Autocomplete() {
-    var dataList = document.getElementById("LänderListe");
-    var input = document.getElementById("LänderEingabe");
-    input.placeholder = "Ländername";
-    for (var i = 0; i < Länder.length; i++) {
-        dataList.innerHTML = dataList.innerHTML +
-            "<option value=" + Länder[i].name + "></option>";
-    }
-}
-
-
-
-// länderauswahl        -------------------------------
-function LandAuswahl() {
-    var num = Math.floor(Math.random() * (imgArr.length));
-    document.canvas.src = "img/" + imgArr[num];
-}
 
 
 
--- main.js
+++ main.js
@@ -9,12 +9,69 @@ chk.addEventListener("change", () => {
 // infopage     --------------------------------------
 function InfopageToggle() {
     var Info = document.getElementById("Infopage");
-    var Main = document.getElementById("Mainpage");
-    if (Info.style.visibility == "hidden") {
-        Info.style.visibility = "visible";
-        Main.style.visibility = "hidden";
-    } else {
-        Info.style.visibility = "hidden";
-        Main.style.visibility = "visible";
+    var Main = document.getElementById("Mainpage")
+    var infoVisible = Info.style.visibility == "visible"
+    Info.style.visibility = infoVisible ? "hidden" : "visible"
+    Main.style.visibility = infoVisible ? "visible" : "hidden"
+}
+
+
+
+// svg array
+var imgArr = []
+var NameArr = []
+for (var i = 0; i < Länder.length; i++) {
+    imgArr[i] = Länder[i].code + ".svg"
+    NameArr[i] = Länder[i].name
+}
+
+// laden der funktionen 
+window.onload = Autocomplete(), LandAuswahl()
+
+
+
+// datenlisten optionen     --------------------------------------
+function Autocomplete() {
+    var dataList = document.getElementById("LänderListe");
+    var input = document.getElementById("LänderEingabe");
+    input.placeholder = "Ländername";
+    for (var i = 0; i < Länder.length; i++) {
+        dataList.innerHTML = dataList.innerHTML +
+            "<option value=" + Länder[i].name + "></option>";
     }
 }
+
+
+
+// länderauswahl        -------------------------------
+var num
+
+function LandAuswahl() {
+    num = Math.floor(Math.random() * (imgArr.length));
+    document.canvas.src = "img/" + imgArr[num];
+    while (Länge > 0) {
+        Länge = Länge - 1
+        document.getElementById("VersucheTabelle").deleteRow(Länge);
+    }
+}
+
+
+
+// Einfügen der Länder in den Table
+const LänderEingabe = document.getElementById("LänderEingabe");
+var Länge = 0
+LänderEingabe.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && Länge < 6) {
+        var tabelle = document.getElementById("VersucheTabelle");
+        var row = tabelle.insertRow();
+        var cell = row.insertCell();
+        var Versuch = document.getElementById("LänderEingabe")
+        cell.innerHTML = Versuch.value
+        Länge = tabelle.rows.length
+        if (Versuch == Länder[num].name) {
+            alert("richtig es ist " + Länder[num].name)
+        } else {
+            alert("falsch es ist " + Länder[num].name)
+        }
+    }
+});
